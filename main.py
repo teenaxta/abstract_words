72a5de2b20b88c047a645c1e39e9305c2ac4c344
teenaxta <manzoor.rohan97@gmail.com>
2024-01-08 00:36:55 +0500

    Added multiprocessing

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ import utils
 import pandas as pd
 import os
 from tqdm import tqdm
+import concurrent.futures
+
+
+def get_word_sets(title: str, paper_url:str) -> (dict, dict):
+    abstract_word_count_dict={}
+    title_word_count_dict={}
+    
+    abstract = utils.get_paper_abstract(paper_url)
+
+    abstract_word_count_set = utils.get_word_count(abstract.split())
+    title_word_count_set = utils.get_word_count(title.split())
+    
+    return abstract_word_count_set, title_word_count_set
+
+def get_word_sets_wrapper(args):
+    return tuple(get_word_sets(*args))
 
 def main(file_path: str) -> dict:
     
@@ -11,32 +27,35 @@ def main(file_path: str) -> dict:
             
             url = line.strip()
             urls.append(url)
+            
+        abstract_word_count_dict={}
+        title_word_count_dict={}
 
         for url in urls:
             titles, page_urls = utils.get_paper_titles_and_urls(url)
             conference_name = utils.get_conference_name(url)
+            conference_year = url[34:38]
             
-            print(f'Currently scrapping {conference_name}')
+            print(f'Currently scrapping {conference_name} of year {conference_year}')
 
-            abstract_word_count_dict={}
-            title_word_count_dict={}
+            # Create a list of tuples pairing titles and page_urls
+            data = list(zip(titles, page_urls))
+            
+            with concurrent.futures.ProcessPoolExecutor(max_workers=40) as executor:
+                results = (executor.map(get_word_sets_wrapper, (data)))
 
-            for title, paper_url in tqdm(zip(titles, page_urls)):
+            print('Complete')
+            out1, out2 = zip(*results)
+            for abstract_word_count_set, title_word_count_set in zip(out1, out2):
+                abstract_word_count_dict = utils.update_dict_with_set\
+                    (abstract_word_count_dict, abstract_word_count_set)
+                title_word_count_dict = utils.update_dict_with_set\
+                    (title_word_count_dict, title_word_count_set)
 
-                abstract = utils.get_paper_abstract(paper_url)
-
-                abstract_word_count = utils.get_word_count(abstract)
-                title_word_count = utils.get_word_count(title)
-
-                abstract_word_count_dict = utils.compare_and_add_dicts\
-                    (abstract_word_count_dict, abstract_word_count)
-                title_word_count_dict = utils.compare_and_add_dicts\
-                    (title_word_count_dict, title_word_count)
-
-            utils.make_csv_from_dict(abstract_word_count_dict, f'results/abstract/{conference_name}')     
-            utils.make_csv_from_dict(abstract_word_count_dict, f'results/title/{conference_name}')
+            utils.make_csv_from_dict(abstract_word_count_dict, f'results/abstract/{conference_name}_{conference_year}.csv')     
+            utils.make_csv_from_dict(title_word_count_dict, f'results/title/{conference_name}_{conference_year}.csv')
 
 
 if __name__=='__main__':
-    file_path = 'urls4.txt'
+    file_path = 'urls.txt'
     main(file_path)
--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ from nltk.tokenize import word_tokenize
 from collections import Counter
 import pandas as pd
 import os
+from matplotlib import pyplot as plt
 
 def get_paper_titles_and_urls(url: str) -> (list, list):
     """Get paper titles and urls given a link of the cvf conference page
@@ -60,7 +61,7 @@ def get_paper_abstract(url: str) -> str:
 
     return abstract
 
-def get_word_count(text: str) -> list:
+def get_word_count(text: str, unique_words_only=True) -> list:
     """Get word count in text without stopwords 
 
     Args:
@@ -74,11 +75,25 @@ def get_word_count(text: str) -> list:
     # nltk.download('stopwords')
 
     stop_words = set(stopwords.words('english'))
-    unique_words = [word.lower() for word in text if word.lower() not in stop_words and word.strip()]  # Filter out stopwords and get unique words
-
+    unique_words = set([word.lower() for word in text if word.lower() not in stop_words and word.strip()])  # Filter out stopwords and get unique words
+    
+    if unique_words_only:
+        return unique_words
+    
     word_counter = dict(Counter(unique_words))
-
     return word_counter
+
+def update_dict_with_set(word_dict, word_set):
+    for word in word_set:
+        if word in word_dict:
+            # If the word exists in the dictionary, increment its count
+            word_dict[word] += 1
+        else:
+            # If the word doesn't exist in the dictionary, add it with a count of 1
+            word_dict[word] = 1
+            
+    return word_dict
+
 
 def compare_and_add_dicts(dict1: dict, dict2: dict) -> dict:
     """Compare and add dictionaries
@@ -125,9 +140,12 @@ def get_conference_name(url: str) -> str:
     return result
 
 def merge_dataframes(df1, df2):
-
+    
+    df1['Words'] = df1['Words'].astype(str)
+    df2['Words'] = df2['Words'].astype(str)
+    
     # Merge dataframes to combine common fruits
-    merged = pd.merge(df1, df2, on='Fruit', how='outer', suffixes=('_df1', '_df2'))
+    merged = pd.merge(df1, df2, on='Words', how='outer', suffixes=('_df1', '_df2'))
 
     # Replace NaN values with 0 for Count columns and add them together
     merged['Count'] = merged['Count_df1'].fillna(0) + merged['Count_df2'].fillna(0)
@@ -140,16 +158,25 @@ def merge_dataframes(df1, df2):
 def make_csv_from_dict(dict1: dict, csv_path: str) -> None:
     dict1 = {
         'Words':list(dict1.keys()),
-        'Counts':list(dict1.values())
+        'Count':list(dict1.values())
     }
     
     if os.path.exists(csv_path):
         df1 = pd.read_csv(csv_path)
         df2 = pd.DataFrame(dict1)
         
-        df1 = utils.merge_dataframes(title_df1, title_df2)        
+        df1 = merge_dataframes(df1, df2)        
     else:
         df1 = pd.DataFrame(dict1)
 
     df1.to_csv(csv_path, index=False)
         
+def plot_df(df, title):
+    plt.figure(figsize=(8, 6))  # Set the figure size (width, height)
+    plt.bar(df['Words'], df['Count'], color='skyblue')  # Create a bar plot
+    plt.xlabel('Words')  # Label for x-axis
+    plt.ylabel('Counts')  # Label for y-axis
+    plt.title(title)  # Title of the plot
+    plt.xticks(rotation=45)  # Rotate x-axis labels for better visibility if needed
+    plt.tight_layout()  # Adjust layout to prevent clipping of labels
+    plt.show()  # Show the plot
